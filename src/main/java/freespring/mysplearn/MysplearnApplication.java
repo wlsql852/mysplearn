badb63d13368e81d6204b02c1f7dc35483e2ade7
wlsql852@naver.com <wlsql852@naver.com>
2025-08-09 01:37:49 +0900

    init

---
+++ src/main/java/freespring/mysplearn/MysplearnApplication.java
@@ -0,0 +1,13 @@
+package freespring.mysplearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysplearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysplearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/freespring/mysplearn/MysplearnApplicationTests.java
@@ -0,0 +1,13 @@
+package freespring.mysplearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysplearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
